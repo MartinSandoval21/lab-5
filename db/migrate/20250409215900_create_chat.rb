c854f6bd252bbffda552dc212be72fbd601b33ed
MartinSandoval21 <masandoval4@miuandes.cl>
2025-04-09 18:25:59 -0400

    Create Tables User, Chat and Message

---
+++ db/migrate/20250409215900_create_chat.rb
@@ -0,0 +1,10 @@
+class CreateChat < ActiveRecord::Migration[8.0]
+  def change
+    create_table :chats do |t|
+      t.integer :sender_id
+      t.integer :receiver_id
+      t.timestamps
+    end
+  end
+end
+
---
+++ db/migrate/20250409215924_create_message.rb
@@ -0,0 +1,10 @@
+class CreateMessage < ActiveRecord::Migration[8.0]
+  def change
+    create_table :messages do |t|
+      t.integer :chat_id
+      t.integer :user_id
+      t.text :body
+      t.timestamps
+    end
+  end
+end
