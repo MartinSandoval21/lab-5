51c45e204830088c38bbcf17d4adadc538e24bd4
MartinSandoval21 <masandoval4@miuandes.cl>
2025-04-09 22:15:51 -0400

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do |i|
+  User.create(email: "user#{i}@gmail.com", first_name: "User", last_name: "#{i}")
+end
+  
+
+10.times do |i|
+  Chat.create(sender_id: i, receiver_id: i + 1000)
+end
+
+10.times do |i|
+  Message.create(chat_id: i+6658, user_id: i+8756, body: " Example #{i}")
+end
+  
